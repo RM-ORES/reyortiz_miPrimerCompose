be145522c5583ea034ac507a9777f61042cb2d04
2dam <rmopix@gmail.com>
2024-01-23 11:11:03 +0100

    clase

--- app/src/androidTest/java/ui/MainActivity.kt
+++ app/src/androidTest/java/ui/MainActivity.kt
@@ -11,13 +11,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -30,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,14 +70,46 @@ fun Pantalla(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                CreateTextBox(label = "Nombre", content = "hola")
+                TextField(value = "-", onValueChange = {}, label = { Text(text = "Nombre")})
+                val datePickerState = rememberDatePickerState(initialSelectedDateMillis = 1578096000000)
+                DatePicker(state = datePickerState, modifier = Modifier.padding(16.dp))
+                val (checkedState, onStateChange) = remember { mutableStateOf(true) }
                 Row(
-                    modifier = Modifier.padding(10.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Checkbox(
+                        checked = checkedState,
+                        onCheckedChange = null
+                    )
+                    Text(
+                        text = "Es legal",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(start = 10.dp)
+                    )
+                }
+
+                RangeSlider(value = 0f..10f,
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    onValueChange = {})
+
+                Row(
+                    modifier = Modifier.padding(5.dp),
                     verticalAlignment = Alignment.Bottom,
                 ) {
                     CreateButton(text = "Add", onClick = { /*TODO*/ })
                     CreateButton(text = "Delete", onClick = { /*TODO*/ })
                     CreateButton(text = "Update", onClick = { /*TODO*/ })
+                }
+                Row(
+                    modifier = Modifier.padding(5.dp),
+                    verticalAlignment = Alignment.Bottom,
+                ){
+                    CreateButton(text = "<", onClick = { /*TODO*/ })
+                    CreateButton(text = ">", onClick = { /*TODO*/ })
                 }
 
             }
@@ -80,22 +117,11 @@ fun Pantalla(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreateTextBox(label: String, content: String) {
-    Row(
-        modifier = Modifier.padding(10.dp),
-    ) {
-        Text(text = label)
-        TextField(value = content, onValueChange = {})
-    }
-}
-
-@Composable
-fun CreateButton(text: String, onClick: () -> Unit) {
+fun CreateButton(text: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
     Button(
         onClick = onClick,
-        modifier = Modifier.padding(10.dp),
+        modifier = modifier.padding(5.dp),
     ) {
         Text(text = text)
     }
